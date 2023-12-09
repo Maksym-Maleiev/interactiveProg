80a85763705c0b1522ca065c701f54d3cca34c98
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-12-09 19:18:31 +0200

    Update interactive.js

--- js/interactive.js
+++ js/interactive.js
@@ -13,6 +13,7 @@ var timeoutId = setTimeout(doHomeworkAlarm, 60000);
 clearTimeout(timeoutId);
 
 // -----------------
+/*
 var counter = 1;
 
 var printMessage = function () {
@@ -22,5 +23,12 @@ var printMessage = function () {
 
 var intervalId = setInterval(printMessage, 1000);
 clearInterval(intervalId);
+*/
 
 // -----------------
+var counter = 5;
+
+var printMessage = function () {
+  console.log("You have been staring at your console for " + counter + " seconds");
+  counter++
+};
