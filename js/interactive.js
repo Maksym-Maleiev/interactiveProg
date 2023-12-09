4235a2ff84d6b726a3ddc6f7472f5851eec3565b
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-12-09 19:28:33 +0200

    Update interactive.js

--- js/interactive.js
+++ js/interactive.js
@@ -30,8 +30,8 @@ var counter = 5;
 
 var printMessage = function () {
   console.log("You have been staring at your console for " + counter + " seconds");
-  counter++;
+  counter += 5;
 };
 
 var intervalId = setInterval(printMessage, 5000);
-clearInterval(intervalId);
+// clearInterval(intervalId);
