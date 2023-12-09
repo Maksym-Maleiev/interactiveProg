05aaba81e1e286a9593901845364eb14924be21f
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-12-09 19:52:47 +0200

    Update interactive_anim.js

--- js/interactive_anim.js
+++ js/interactive_anim.js
@@ -3,7 +3,7 @@ var leftOffset = 0;
 var moveHeading = function () {
   $("#heading").offset({ left: leftOffset });
 
-  leftOffset++;
+  leftOffset += 4;
 
   if (leftOffset > 200) {
     leftOffset = 0;
