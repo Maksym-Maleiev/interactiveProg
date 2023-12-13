e84a30e02adb699076bfe5c0d7cb5a1561afc967
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-12-13 14:58:38 +0200

    Update mousemove.js

--- js/mousemove.js
+++ js/mousemove.js
@@ -22,11 +22,16 @@ var topOffset = 0;
 function moveHeading() {
   $("#heading").offset({ left: leftOffset, top: topOffset});
 
-  for (var leftOffset = 0; leftOffset < 200; leftOffset++){
+  leftOffset++;
+
+  topOffset++;
+
+  if (leftOffset > 200) {
+    leftOffset = 200;
+  } else {
+    topOffset = 200;
   }
-  
-  for (var topOffset = 0; topOffset < 200; topOffset++){
- }
+
 };
 
 setInterval(moveHeading, 10);
