1728e7ec183b249d758c44e0386e05e8a7915656
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-12-13 15:50:23 +0200

    Create interactiveHw.js

---
+++ js/interactiveHw.js
@@ -0,0 +1,33 @@
+// #1: follow the clicks
+/*
+$("html").click(function (event) {
+  $("#heading").offset({
+    left: event.pageX,
+    top: event.pageY,
+  });
+});
+*/
+
+// #2:create your own animation
+var leftOffset = 0;
+var topOffset = 0;
+
+function moveHeading() {
+  $("#heading").offset({ left: leftOffset, top: topOffset});
+
+  leftOffset++;
+
+  if (leftOffset > 200) {
+    leftOffset = 200;
+  } else {
+    topOffset = 0;
+  }
+
+  topOffset++;
+
+  if (topOffset > 200) {
+    topOffset = 200;
+  }
+};
+
+setInterval(moveHeading, 10);
