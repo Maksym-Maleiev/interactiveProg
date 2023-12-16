93603318dc8b19d31f055ff8bb0b42dee4b8a65d
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-12-16 16:36:11 +0200

    Update interactiveHw.js

--- js/interactiveHw.js
+++ js/interactiveHw.js
@@ -56,25 +56,25 @@ var intervalId = setInterval(moveHeading, intervalTime);
 
 // #3: cancel the animation with a click
 
-$("#heading").click(function () {
-  clearInterval(intervalId);
-});
+// $("#heading").click(function () {
+//   clearInterval(intervalId);
+// });
 
 // #4: create a "title clicker" game
 
-// var clickNumber = 0;
-// var newHeading = "You Win🎉";
+var clickNumber = 0;
+var newHeading = "You Win🎉";
 
-// $("#heading").click(function clickHeading() {
-//   for (var i = 0; i < 1; i++){
-//     if (clickNumber === 4) {
-//       $("#heading").text(newHeading);
-//       clearInterval(intervalId);
-//     } else {
-//     setInterval(moveHeading, intervalTime++);
-//     clickNumber++;
+$("#heading").click(function () {
+  for (var i = 0; i < 1; i++){
+    if (clickNumber === 4) {
+      $("#heading").text(newHeading);
+      clearInterval(intervalId);
+    } else {
+    setInterval(moveHeading, intervalTime++);
+    clickNumber++;
 
-//     console.log("Click " + clickNumber + "!");     
-//       }
-//     }
-//   })
+    console.log("Click " + clickNumber + "!");     
+      }
+    }
+  })
