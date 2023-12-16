cdd1c08ef0356ad134007c2bc2db0e6840596112
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-12-16 15:01:15 +0200

    Update interactiveHw.js

--- js/interactiveHw.js
+++ js/interactiveHw.js
@@ -62,6 +62,8 @@ var intervalId = setInterval(moveHeading, 18);
 
 // #4: create a "title clicker" game
 
-$("#heading").click(function moveFaster () {
-  
+$("#heading").click(function (event) {
+  for (var event = 0; event < 10; event++ ) {
+    
+  }
 })
